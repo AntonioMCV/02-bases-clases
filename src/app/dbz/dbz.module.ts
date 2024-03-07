445cd110f74ca1cc7034f8bf5a002c83cd962af8
Antonio Martinez Perez <antonio.martinez.p@ricoh.es>
2024-03-07 16:28:06 +0100

    prueba dbz reinicio del add-character

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainPage } from './pages/dbz.main-page.component';
+import { MainPage } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
 import { AddCharacterComponent } from './components/add-character/add-character.component';
 
